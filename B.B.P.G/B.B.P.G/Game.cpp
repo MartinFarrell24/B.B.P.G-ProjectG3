cb7ca21e67421031da9569447abe1fc9d1f0e39c
GregCahillC00216328 <C00216328@itcarlow.ie>
2018-11-01 15:10:55 +0000

    Test

--- B.B.P.G/B.B.P.G/Game.cpp
+++ B.B.P.G/B.B.P.G/Game.cpp
@@ -1,6 +1,9 @@
 #include "Game.h"
+GameState Game::m_currentMode{ GameState::intro };
 
-Game::Game()
+Game::Game() :
+m_window{sf::VideoMode{ 800, 600, 32 }, "Mini-Motivators"},
+m_exitGame{ false } //when true game will exit
 {
 }
 
--- B.B.P.G/B.B.P.G/Game.h
+++ B.B.P.G/B.B.P.G/Game.h
@@ -4,18 +4,25 @@
 #include"Intro.h"
 #include"MainMenu.h"
 #include"Splash.h"
-//#include"SFML/Graphics.hpp"
+#include"SFML/Graphics.hpp"
 
 class Game
 {
 public:
 	Game();
 	~Game();
+	/// <summary>
+	/// main method for game
+	/// </summary>
+	void run();
+	// used to determin which screen we're on
+	static GameState m_currentMode;
 
 	void update();
 	void render();
 	void loadAssets();
 	sf::RectangleShape rectyboy;
+	GameState current_state;
 
 private:
 	Player m_player;
